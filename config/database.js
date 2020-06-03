845f0547d7b640204f94392c23ddc3d634a480e2
Vanessa Larsen <vanessalarsen@Vanessas-MacBook-Air.local>
2020-06-03 12:59:04 -0600

    mongodb uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
 // mongoose.connect('mongodb://localhost/mood-tracker', {
   useNewUrlParser: true,
     useUnifiedTopology: true,
